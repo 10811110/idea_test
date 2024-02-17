e30dbd07a6e1ecd9919fb301a23c016339b35737
ben900415 <ben900415@gmail.com>
2024-02-17 20:39:07 +0800

    add 數據容器

--- test.py
+++ test.py
@@ -1,2 +1,19 @@
-print("hello world")
 str1 = "hello world"
+print(str1)
+print(type(str1))
+
+set1 = {"123","456"}
+print(set1)
+print(type(set1))
+
+tuple1 = (1,"hello",True)
+print(tuple1)
+print(type(tuple1))
+
+list1 = [1,2,[3,4]]
+print(list1)
+print(type(list1))
+
+dict1 = {"123": 1, "456": 2}
+print(dict1)
+print(type(dict1))
